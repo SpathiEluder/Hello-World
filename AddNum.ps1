467dd84556bb70822310f27561fc58cc5e7fb71c
SpathiEluder <bower-mark@outlook.com>
2017-07-18 11:17:04 -0400

    Add files via upload

---
+++ AddNum.ps1
@@ -0,0 +1,8 @@
+﻿$AddNumbers = Import-Csv -Delimiter "," -Path ".\AddText.csv"            
+$total = 0
+
+foreach ($Number in $AddNumbers)            
+{
+    $total = $total + $Number.Input
+    Write-host "The Total so far is " $total
+}
